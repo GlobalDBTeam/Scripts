2848d80ca573b1cbd74ea940b5ceeb45628a2fa9
krogacion <katherine.rogacion@transcom.com>
2022-01-11 22:14:19 +0800

    Updated backup control file script. Reverted to the original date format.

--- SNLROPWFMDB002/backup_control_file.sql
+++ SNLROPWFMDB002/backup_control_file.sql
@@ -1,6 +1,6 @@
 col filedate new_value fdate
 col filename new_value fname
-select to_char(sysdate,'DDMMYYYY') as filedate from dual;
+select to_char(sysdate,'MMDDYYYY') as filedate from dual;
 select 'E:\script\backup_controlfile\control_file_' || &&fdate || '.bkp' as filename from dual;
 ALTER DATABASE BACKUP CONTROLFILE TO TRACE AS '&&fname';
 exit;
