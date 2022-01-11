e5d6eace06892cb2d18b38d104cad99596c36a52
GlobalDBTeam <97520888+GlobalDBTeam@users.noreply.github.com>
2022-01-11 22:37:21 +0800

    Delete backup_control_file.sql

--- SNLROPWFMDB002/backup_control_file.sql
+++
@@ -1,6 +0,0 @@
-col filedate new_value fdate
-col filename new_value fname
-select to_char(sysdate,'MMDDYYYY') as filedate from dual;
-select 'E:\script\backup_controlfile\control_file_' || &&fdate || '.bkp' as filename from dual;
-ALTER DATABASE BACKUP CONTROLFILE TO TRACE AS '&&fname';
-exit;
