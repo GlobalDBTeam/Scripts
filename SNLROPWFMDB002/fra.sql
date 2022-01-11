3c964b8327268ae4518a87eb224cc3c488f9872c
GlobalDBTeam <97520888+GlobalDBTeam@users.noreply.github.com>
2022-01-11 22:38:48 +0800

    Delete fra.sql

--- SNLROPWFMDB002/fra.sql
+++
@@ -1,9 +0,0 @@
-set lines 150
-col name format a80
-set head on 
-set feed on
-select name from v$database;
-select to_char(sysdate,'DD-MON-YYYY HH24:MI:SS') from dual;
-SELECT NAME, ROUND(SPACE_LIMIT / 1048576) SPACE_LIMIT_MB, ROUND(SPACE_USED / 1048576) SPACE_USED_MB,
-ROUND(((SPACE_USED / 1048576) * 100) / (SPACE_LIMIT / 1048576), 2) PRC_USED FROM V$RECOVERY_FILE_DEST;
-exit;
